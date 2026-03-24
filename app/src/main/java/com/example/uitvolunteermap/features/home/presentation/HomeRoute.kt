d0f6e3faac154fd96e25a21e708ba697cc95082f
nphng15 <phong98753@gmail.com>
2026-03-24 11:58:32 +0700

    feat: map usecase flow to homescreen

--- app/src/main/java/com/example/uitvolunteermap/features/home/presentation/HomeRoute.kt
+++ app/src/main/java/com/example/uitvolunteermap/features/home/presentation/HomeRoute.kt
@@ -1,9 +1,13 @@
 package com.example.uitvolunteermap.features.home.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
-fun HomeRoute() {
+fun HomeRoute(viewModel: HomeViewModel = hiltViewModel()) {
+    val state = viewModel.uiState.collectAsStateWithLifecycle()
+
     HomeScreen(
         state = HomeUiState()
     )
