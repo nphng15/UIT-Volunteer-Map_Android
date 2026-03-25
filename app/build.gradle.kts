1ecffaa1b8c9ea715e7f37b738f7430331a2fc6f
nphng15 <phong98753@gmail.com>
2026-03-25 16:03:09 +0700

    [UVM-010.01] fix(home): resolve dashboard route and dependency issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.hilt.navigation.compose)
     implementation(libs.coroutines.android)
     implementation(libs.hilt.android)
     implementation(libs.material)
