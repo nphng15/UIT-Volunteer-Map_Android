85ea9c999711060b155d353fe6cbd40862cfbd3c
ThanhThang5722 <23521427@gm.uit.edu.vn>
2026-03-29 22:35:10 +0700

    init guest homepage

---
+++ app/src/main/java/com/example/uitvolunteermap/features/home/presentation/guest/GuestHomeViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.uitvolunteermap.features.home.presentation.guest
+
+import com.example.uitvolunteermap.features.home.data.repository.CampaignRepository
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GuestHomeViewModel @Inject constructor(
+    private val repository: CampaignRepository
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(GuestHomeUiState())
+    val uiState: StateFlow<GuestHomeUiState> = _uiState
+
+    init {
+        fetchCampaigns()
+    }
+
+    private fun fetchCampaigns() {
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(isLoading = true)
+
+            try {
+                val response = repository.getCampaigns()
+
+                val campaigns = response.data.map {
+                    CampaignUiModel(
+                        id = it.campaignId,
+                        name = it.campaignName,
+                        description = it.description,
+                        startDate = it.startDate,
+                        endDate = it.endDate
+                    )
+                }
+
+                _uiState.value = GuestHomeUiState(
+                    campaigns = campaigns
+                )
+
+            } catch (e: Exception) {
+                _uiState.value = GuestHomeUiState(
+                    error = e.message
+                )
+            }
+        }
+    }
+}
