d68602fe944666796f7ba51d40455e844ca83e9b
hienhunghang <hien.thanh2711@gmail.com>
2026-04-12 03:30:12 +0700

    [UVM-015.01] feat(profile): add profile screen UI

--- app/src/main/java/com/example/uitvolunteermap/app/MainActivity.kt
+++ app/src/main/java/com/example/uitvolunteermap/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.uitvolunteermap.core.ui.theme.UITVolunteerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
---
+++ app/src/main/java/com/example/uitvolunteermap/features/profile/presentation/components/ProfileTopAppBar.kt
@@ -0,0 +1,41 @@
+package com.example.uitvolunteermap.features.profile.presentation.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileTopAppBar(
+    onBackClick: () -> Unit,
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Hồ sơ cá nhân",
+                style = MaterialTheme.typography.titleLarge,
+                maxLines = 1,
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Back"
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            titleContentColor = MaterialTheme.colorScheme.onSurface,
+            navigationIconContentColor = MaterialTheme.colorScheme.onSurface,
+        )
+    )
+}
