3b0c1bdf2b89840759de82c8735c6ba503347234
Fongg <46365986+nphng15@users.noreply.github.com>
2026-04-13 19:55:13 +0700

    Revert "Feature profile"

--- app/src/main/java/com/example/uitvolunteermap/app/MainActivity.kt
+++ app/src/main/java/com/example/uitvolunteermap/app/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.uitvolunteermap.core.ui.theme.UITVolunteerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
