eb31f2d1b1931b06149135c2aa969b55ffe31ff2
nphng15 <phong98753@gmail.com>
2026-06-13 14:00:00 +0700

    [UVM-035.08] refactor(cleanup): remove deprecated screens and GPS checkin components Delete deprecated home screens: - features/home/presentation/HomeRoute.kt - features/home/presentation/HomeScreen.kt - features/home/presentation/HomeViewModel.kt - features/home/presentation/HomeUiState.kt - features/home/presentation/guest/GuestHomeRoute.kt - features/home/presentation/guest/GuestHomeScreen.kt - features/home/presentation/guest/GuestHomeViewModel.kt - features/home/presentation/guest/GuestHomeUiState.kt
    Delete GPS checkin feature:
    - features/checkin/presentation/GpsCheckinRoute.kt
    - features/checkin/presentation/GpsCheckinScreen.kt
    - features/checkin/presentation/GpsCheckinViewModel.kt
    - features/checkin/presentation/GpsCheckinUiState.kt
    - features/checkin/presentation/GpsCheckinUiEvent.kt
    - features/checkin/presentation/GpsCheckinUiEffect.kt
    - features/checkin/presentation/components/CheckinResultContent.kt
    - features/checkin/presentation/components/CheckingInContent.kt
    - features/checkin/presentation/components/HistorySection.kt
    - features/checkin/presentation/components/IdleContent.kt
    - features/checkin/presentation/components/LoadingLocationContent.kt
    - features/checkin/presentation/components/LocationReadyContent.kt
    - features/checkin/presentation/components/GpsCheckinTokens.kt
    
    Delete team formation component:
    - features/campaign/presentation/team/components/CheckinButton.kt
    
    Replaced by:
    - VolunteerHome as primary volunteer landing screen
    - CheckinHub for volunteer check-in operations
    - Admin role-based dashboard

--- app/src/main/java/com/example/uitvolunteermap/features/home/presentation/HomeUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.example.uitvolunteermap.features.home.presentation
-
-data class HomeUiState(
-    val title: String = "",
-    val subtitle: String = "",
-    val isLoading: Boolean = false,
-    val errorMessage: String? = null
-)
