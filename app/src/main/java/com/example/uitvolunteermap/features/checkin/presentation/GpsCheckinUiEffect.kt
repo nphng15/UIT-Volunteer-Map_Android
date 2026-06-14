f9bf944d13673f32593b64f623a9aac392cc9db2
nphng15 <phong98753@gmail.com>
2026-06-14 20:05:18 +0700

    refactor(cleanup): remove deprecated screens and GPS checkin components
    Delete deprecated home screens:
    - features/home/presentation/HomeRoute.kt
    - features/home/presentation/HomeScreen.kt
    - features/home/presentation/HomeViewModel.kt
    - features/home/presentation/HomeUiState.kt
    - features/home/presentation/guest/GuestHomeRoute.kt
    - features/home/presentation/guest/GuestHomeScreen.kt
    - features/home/presentation/guest/GuestHomeViewModel.kt
    - features/home/presentation/guest/GuestHomeUiState.kt
    
    Delete GPS checkin feature:
    - features/checkin/presentation/GpsCheckinRoute.kt
    - features/checkin/presentation/GpsCheckinScreen.kt
    - features/checkin/presentation/GpsCheckinViewModel.kt
    - features/checkin/presentation/GpsCheckinUiState.kt
    - features/checkin/presentation/GpsCheckinUiEvent.kt
    - features/checkin/presentation/GpsCheckinUiEffect.kt
    - features/checkin/presentation/components/CheckinResultContent.kt
    - features/checkin/presentation/components/CheckingInContent.kt
    - features/checkin/presentation/components/HistorySection.kt
    - features/checkin/presentation/components/IdleContent.kt
    - features/checkin/presentation/components/LoadingLocationContent.kt
    - features/checkin/presentation/components/LocationReadyContent.kt
    - features/checkin/presentation/components/GpsCheckinTokens.kt
    
    Delete team formation component:
    - features/campaign/presentation/team/components/CheckinButton.kt
    
    Replaced by:
    - VolunteerHome as primary volunteer landing screen
    - CheckinHub for volunteer check-in operations
    - Admin role-based dashboard

--- app/src/main/java/com/example/uitvolunteermap/features/checkin/presentation/GpsCheckinUiEffect.kt
+++
@@ -1,8 +0,0 @@
-package com.example.uitvolunteermap.features.checkin.presentation
-
-sealed interface GpsCheckinUiEffect {
-    data object NavigateBack : GpsCheckinUiEffect
-    data object RequestLocationPermission : GpsCheckinUiEffect
-    data object RequestLocation : GpsCheckinUiEffect
-    data class ShowMessage(val message: String) : GpsCheckinUiEffect
-}
